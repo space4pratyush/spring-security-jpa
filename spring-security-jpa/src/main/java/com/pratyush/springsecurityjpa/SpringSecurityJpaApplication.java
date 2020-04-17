042065f3f5e4f00b0c692b365be40d0836e55e84
space4pratyush <space4pratyush@gmail.com>
2020-04-18 00:53:18 +0530

    first commit

---
+++ spring-security-jpa/src/main/java/com/pratyush/springsecurityjpa/SpringSecurityJpaApplication.java
@@ -0,0 +1,19 @@
+package com.pratyush.springsecurityjpa;
+
+import com.pratyush.springsecurityjpa.repositories.UserRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+public class SpringSecurityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJpaApplication.class, args);
+	}
+
+}
---
+++ spring-security-jpa/src/test/java/com/pratyush/springsecurityjpa/SpringSecurityJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratyush.springsecurityjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
